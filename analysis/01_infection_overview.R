012709b460841aab0a55b72f58c31b0cec1dc2b0
OJWatson <oj.watson@hotmail.co.uk>
2021-01-13 21:53:14 +0000

    new appendix table

--- analysis/01_infection_overview.R
+++ analysis/01_infection_overview.R
@@ -30,14 +30,14 @@ df_summary_species_mixed <- rbind(df_summary_species_mixed,c("Pm/Poc/Pow",0,0,3)
 df_summary_species_mixed$mean <- as.numeric(df_summary_species_mixed$mean)
 df_summary_species_mixed$type <- c("Single","Double","Triple","Quadruple")[df_summary_species_mixed$type]
 df_summary_species_mixed$type <- factor(df_summary_species_mixed$type ,levels=c("Single","Double","Triple","Quadruple"))
-df_summary_species_mixed$ymin <- vapply(as.numeric(df_summary_species_mixed$n), function(x) Hmisc::binconf(x,2027)[2], numeric(1))
-df_summary_species_mixed$ymax <- vapply(as.numeric(df_summary_species_mixed$n), function(x) Hmisc::binconf(x,2027)[3], numeric(1))
+df_summary_species_mixed$ci025 <- vapply(as.numeric(df_summary_species_mixed$n), function(x) Hmisc::binconf(x,2027)[2], numeric(1))
+df_summary_species_mixed$ci975 <- vapply(as.numeric(df_summary_species_mixed$n), function(x) Hmisc::binconf(x,2027)[3], numeric(1))
 
 # fig 1 plot
 mixed_infection <- ggplot(df_summary_species_mixed[rev(order(df_summary_species_mixed$mean)),],
                           aes(x=reorder(species,mean),y=mean,fill=type,color=type)) + geom_bar(stat="identity") +
   geom_text(aes(label=paste0(round(mean,4)*100,"% (",n,"/2027)")) , color="black",hjust = -0.4) +
-  geom_errorbar(aes(ymin = ymin, ymax = ymax, x = species), color = "black", width = 0.25) +
+  geom_errorbar(aes(ymin = ci025, ymax = ci975, x = species), color = "black", width = 0.25) +
   ylab("Percentage Infections") + xlab("Infection Composition") +
   theme_bw() + our_theme + coord_flip() +  scale_y_continuous(labels = scales::percent, limits = c(0,1)) +
   scale_color_brewer(palette = "Paired",name="Infection Type",) +
@@ -45,6 +45,14 @@ mixed_infection <- ggplot(df_summary_species_mixed[rev(order(df_summary_species_
 
 save_figs("fig1", mixed_infection, 9.5, 6)
 
+df_summary_species_mixed$mean <- scales::percent(df_summary_species_mixed$mean, accuracy = 0.01)
+df_summary_species_mixed$ci025 <- scales::percent(df_summary_species_mixed$ci025, accuracy = 0.01)
+df_summary_species_mixed$ci975 <- scales::percent(df_summary_species_mixed$ci975, accuracy = 0.01)
+df_summary_species_mixed <- df_summary_species_mixed[order(as.numeric(df_summary_species_mixed$n), decreasing = TRUE),]
+df_summary_species_mixed$n <- paste0(df_summary_species_mixed$n, "/", sum(as.numeric(df_summary_species_mixed$n)))
+df_summary_species_mixed$mean <- paste0(df_summary_species_mixed$mean, " [", df_summary_species_mixed$ci025, " - ", df_summary_species_mixed$ci975, "]")
+df_summary_species_mixed <- df_summary_species_mixed[,c("species", "type", "mean", "n")]
+write.csv(df_summary_species_mixed, file.path(here::here(), "analysis/tables/figure1_table.csv"), row.names = FALSE)
 
 # ------------------------------------------------------------------------------
 # Supp Figure By Sites
